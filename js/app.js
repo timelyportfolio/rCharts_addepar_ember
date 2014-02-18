5d5c020e4824b3d3534e640fcaba0b01e12108fe
timelyportfolio <kent.russell@timelyportfolio.com>
2014-02-17 21:30:58 -0600

    copied from addepar example but still no luck;  table is there but no data even though I see data in controller

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // copied most from Addepar
-(function () {
+
 
   window.App = Ember.Application.create({
     customEvents: {
@@ -7,34 +7,60 @@
     }
   });
 
-  App.ApplicationView = Ember.View.extend({
-    classNames: 'ember-app',
-    templateName: 'application'
-  });
 
-  App.ApplicationController = Ember.Controller.extend({
-    tableSimpleExampleController: Ember.computed(function () {
-      return Ember.get('App.TableSimpleExample.TableController').create();
-    }).property()
-  });
-
-  App.Router.map(function() {
-    this.resource('simple', { path: '/' });
-  });
-
-  App.TableSimpleExample = Ember.Namespace.create();
-
-  App.LazyDataSource = Ember.ArrayProxy.extend({
-    objectAt: function(idx) {
-      var date, row;
-      row = this.get('content')[idx];
-      if (row) {
-        return row;
+App.EmberTableSimpleController = Ember.Controller.extend({
+  numRows: 100,
+  columns: Ember.computed(function() {
+    var closeColumn, dateColumn, highColumn, lowColumn, openColumn;
+    dateColumn = Ember.Table.ColumnDefinition.create({
+      columnWidth: 150,
+      textAlign: 'text-align-left',
+      headerCellName: 'Date',
+      getCellContent: function(row) {
+        return row['date'].toDateString();
       }
+    });
+    openColumn = Ember.Table.ColumnDefinition.create({
+      columnWidth: 100,
+      headerCellName: 'Open',
+      getCellContent: function(row) {
+        return row['open'].toFixed(2);
+      }
+    });
+    highColumn = Ember.Table.ColumnDefinition.create({
+      columnWidth: 100,
+      headerCellName: 'High',
+      getCellContent: function(row) {
+        return row['high'].toFixed(2);
+      }
+    });
+    lowColumn = Ember.Table.ColumnDefinition.create({
+      columnWidth: 100,
+      headerCellName: 'Low',
+      getCellContent: function(row) {
+        return row['low'].toFixed(2);
+      }
+    });
+    closeColumn = Ember.Table.ColumnDefinition.create({
+      columnWidth: 100,
+      headerCellName: 'Close',
+      getCellContent: function(row) {
+        return row['close'].toFixed(2);
+      }
+    });
+    return [dateColumn, openColumn, highColumn, lowColumn, closeColumn];
+  }),
+  content: Ember.computed(function() {
+    var _i, _ref, _results;
+    return (function() {
+      _results = [];
+      for (var _i = 0, _ref = this.get('numRows'); 0 <= _ref ? _i < _ref : _i > _ref; 0 <= _ref ? _i++ : _i--){ _results.push(_i); }
+      return _results;
+    }).apply(this).map(function(index) {
+      var date;
       date = new Date();
-      date.setDate(date.getDate() + idx);
-      row = {
-        index: idx,
+      date.setDate(date.getDate() + index);
+      return {
         date: date,
         open: Math.random() * 100 - 50,
         high: Math.random() * 100 - 50,
@@ -42,54 +68,7 @@
         close: Math.random() * 100 - 50,
         volume: Math.random() * 1000000
       };
-      this.get('content')[idx] = row;
-      return row;
-    }
-  });
+    });
+  }).property('numRows')
+});
 
-  App.TableSimpleExample.TableController = Ember.Controller.extend({
-    hasHeader: true,
-    hasFooter: false,
-    numFixedColumns: 0,
-    numRows: 500000,
-    rowHeight: 30,
-    columns: Ember.computed(function() {
-      var columnNames, columns, dateColumn, entryColumn;
-      columnNames = ['open', 'high', 'low', 'close', 'volume'];
-      entryColumn = Ember.Table.ColumnDefinition.create({
-        columnWidth: 100,
-        headerCellName: 'Entry',
-        getCellContent: function(row) {
-          return row['index'];
-        }
-      });
-      dateColumn = Ember.Table.ColumnDefinition.create({
-        columnWidth: 150,
-        headerCellName: 'Date',
-        getCellContent: function(row) {
-          return row['date'].toDateString();
-        }
-      });
-      columns = columnNames.map(function(key, index) {
-        var name;
-        name = key.charAt(0).toUpperCase() + key.slice(1);
-        return Ember.Table.ColumnDefinition.create({
-          columnWidth: 100,
-          headerCellName: name,
-          getCellContent: function(row) {
-            return row[key].toFixed(2);
-          }
-        });
-      });
-      columns.unshift(dateColumn);
-      columns.unshift(entryColumn);
-      return columns;
-    }).property(),
-    content: Ember.computed(function() {
-      return App.TableSimpleExample.LazyDataSource.create({
-        content: new Array(this.get('numRows'))
-      });
-    }).property('numRows')
-  });
-
-}).call(this);
