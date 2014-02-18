8ed9f9a8364f9482cfd603b10272b386b2ffc3cb
timelyportfolio <kent.russell@timelyportfolio.com>
2014-02-18 09:59:53 -0600

    fix js bug var instead of bar

--- examples.R
+++ examples.R
@@ -4,7 +4,37 @@
 
 require(rCharts)
 #make a new rCharts object
-aTable <- rCharts$new()
+rChartsAddepar <- setRefClass("rChartsAddepar",
+  contains = "rCharts",
+  methods = list(
+    initialize = function(){
+      callSuper(); 
+    },
+    render = function (chartId = NULL, cdn = F, static = T, standalone = F) {
+        params$dom <<- chartId %||% params$dom
+        template = read_template(getOption("RCHART_TEMPLATE", templates$page))
+        assets = Map("c", get_assets(LIB, static = static, cdn = cdn, 
+           standalone = standalone), html_assets)
+        html = render_template(template, list(params = params, assets = assets, 
+                    chartId = params$dom, script = .self$html(params$dom), 
+                    CODE = srccode, lib = LIB$name, tObj = tObj, container = container), 
+                    partials = list(chartDiv = templates$chartDiv))
+        html = gsub(
+          x = html,
+          pattern  = "</body>",
+          replacement = paste0(
+            templates$afterScript,
+            '
+            </body>
+            '
+          )
+        )
+  })
+)
+aTable <- rChartsAddepar$new()
 aTable$setLib("http://timelyportfolio.github.io/rCharts_addepar_ember")
-aTable$templates$script = "./layouts/simple.hbs"
+aTable$setTemplate(
+  afterScript=""
+)
 
+aTable
